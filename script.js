23901d3037482ad3ed33fb1036b32e391370e5c4
Pyrox004 <abderaghman.calendly@gmail.com>
2025-09-26 16:51:49 +0200

    completed the determineWinner function

--- script.js
+++ script.js
@@ -17,3 +17,18 @@ function getHumanChoice() {
     }
     while (!options.includes(choice))
 }
+
+function determineWinner(humanChoice, computerChoice) {
+    if (humanChoice == computerChoice)
+        return "it's a tie"
+    else if (
+        (humanChoice == options[0] && computerChoice == options[2]) ||
+        (humanChoice == options[1] && computerChoice == options[0]) ||
+        (humanChoice == options[2] && computerChoice == options[1])
+    )
+        return "You win!"
+
+    else 
+        return "Computer Wins"
+    
+}
